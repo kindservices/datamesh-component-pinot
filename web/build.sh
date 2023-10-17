4f1ae0ba0d8de9ac20ce5820c99a6b46a3fcba57
aaron.pritzlaff <aaron@kindservices.co.uk>
2023-10-17 17:42:10 +0100

    Initialised project with a basic server and web component

---
+++ web/build.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+export TAG=${TAG:-local}
+export IMG=${IMG:-kindservices/datamesh-component-pinot-web:$TAG}
+export PORT=${PORT:-3000}
+
+
+build() {
+    echo "Building $IMG..."
+    docker build --tag $IMG .
+    echo "Built $IMG. To run:"
+    echo ""
+    echo "docker run -it -p $PORT:80 $IMG"
+    echo ""
+    echo "And open http://localhost:$PORT/bundle.js or http://localhost:$PORT/bundle.css"
+}
+
+dev() {
+    yarn
+    yarn dev
+}
+
+clean() {
+    yarn cache clean
+}
+
+run() {
+    echo "docker run -it --rm -p $PORT:80 -d $IMG"
+    id=`docker run -it --rm -p $PORT:80 -d $IMG`
+    cat > kill.sh <<EOL
+docker kill $id
+# clean up after ourselves
+rm kill.sh
+EOL
+    chmod +x kill.sh
+
+    echo "Running on port $PORT --- stop server using ./kill.sh"
+}
+
+installArgo() {
+    APP=${APP:-pinot-web}
+    BRANCH=${BRANCH:-`git rev-parse --abbrev-ref HEAD`}
+
+    echo "creating $APP"
+    
+    kubectl create namespace data-mesh || echo "couldn't create data-mesh namespace"
+    
+    # beast mode :-)
+    argocd app create $APP \
+    --repo https://github.com/kindservices/datamesh-component-pinot.git \
+    --path web/k8s \
+    --dest-server https://kubernetes.default.svc \
+    --dest-namespace data-mesh \
+    --sync-policy automated \
+    --auto-prune \
+    --self-heal \
+    --revision $BRANCH
+
+}
