eb5094e4e35db8d89564ffb465465bb26bf81f35
aaron.pritzlaff <aaron@kindservices.co.uk>
2023-11-02 22:02:54 +0000

    Updated readme

--- install.sh
+++ install.sh
@@ -13,11 +13,6 @@ echo "============== installing pinot in `pwd` =============="
 make installArgo
 popd
 
-pushd "$thisDir/pinot-bff"
-echo "============== installing pinot-bff in `pwd` =============="
-make installArgo
-popd
-
 pushd "$thisDir/kafka-test-widget"
 echo "============== installing kafka-test-widget =============="
 make installArgo
@@ -28,3 +23,18 @@ pushd "$thisDir/web"
 echo "============== installing web in `pwd` =============="
 make installArgo
 popd
+
+
+echo "   +-------------------------------------------------------+"
+echo "   | all installed! You'll need to create a kafka topic    |"
+echo "   | and create the pinot schema/table. See the readme.md  |"
+echo "   | for more - which just tells you to run the following  |"
+echo "   | after port-forwarding kafka and pinot controller.     |"
+echo "   +-------------------------------------------------------+"
+echo
+echo
+echo "kafka-topics --create --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1 --topic user-tracking-data"
+echo "curl -F schemaName=@data/schema.json  localhost:9000/schemas"
+echo "curl -i -X POST -H 'Content-Type: application/json' -d @data/table.json localhost:9000/tables"
+echo
+echo
